070c7bc5e983e40b064a65e0f892ef01c3d320d4
Mihai POITELEA <mihaipoitelea11@gmail.com>
2023-03-24 12:05:25 +0100

    start Chord

--- SysDis/chord/chord_tools.py
+++ SysDis/chord/chord_tools.py
@@ -14,3 +14,52 @@ def json_send(ip, port, data):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((ip, port))
         s.send(json.dumps(data).encode())
+
+#-----------------Pour le lookup----------------------------------
+
+def query (ip,port, id):
+    #utilisé par le node qui fait la requete
+    return 0
+
+def lookup (ip,port, id):
+    #utilisé par nodes dans le chord pour chercher id parmis les nodes
+    return 0
+
+def response (id, present, [value]):
+    #utilisé par le noeud qui a le id, present boolean si il y a bien l'id, value optionnel
+
+#--------------------Pour le join----------------------------------
+
+def join(ip, port):
+    return 0
+
+def check(ip, port, id):
+    #call rand() to generate ‘id’ if the first node, then send to the correct node
+    return 0
+
+def nav():
+    #-> “NonAvailable”, sent to the external node asking to join
+    return 0
+
+def ok(ip_pred, port_pred, id_pred, ip_succ, port_succ, id_succ, data):
+    #-> sent to the external node asking join
+    return 0
+
+def update(ip_new, port_new, id_new):
+    #-> sent to the previous node : if that node is impacted, update its TV and send to the previous node again (else: stop)
+    return 0
+
+#--------------------Modif de valeur----------------------------------
+
+def set(key, value):
+    #-> if the node does not own the key, send it to the next node. No acknowledgement
+    return 0
+
+#-----------------------Pour le quit----------------------------------
+
+def quit(nb_query, nb_set, nb_management, ip, port) :
+    #nb_query 	: add to the variable how many ‘query’ and ‘lookup’ the node has used, then send to the next node 
+    #nb_set 	: add to the variable how many ‘set’ the node has used, then send to the next node
+    #nb_management : add to the variable how many of ‘everything else’ the node has used, then send to the next node
+    #ip, port 	: of the external node
+    return 0
--- SysDis/chord/chord_v1.py
+++ SysDis/chord/chord_v1.py
@@ -3,13 +3,19 @@
 import socket
 from chord_tools import *
 
+myIp = '130.120.232.10'
+IpClient = '130.120.232.9'
+host = socket.gethostname()
+print(host)
+
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as serversocket:
     serversocket.bind(('', 8001))
     serversocket.listen(5)
     print('listening on port:', serversocket.getsockname()[1])
     while True:
         (clientsocket, address) = serversocket.accept()
+        print('accept')
         json_data = json_recv(clientsocket)
         print(json_data)
-
         json_send('localhost', 9000, 42)
